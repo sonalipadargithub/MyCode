3061b320906fb2ea34a225972b79338ffcdf8374
hp <hp@DESKTOP-GOVVA5C>
2022-10-03 22:49:21 +0530

    sending Code

---
+++ polymorphisam/src/com/tests/ExampleMain.java
@@ -0,0 +1,16 @@
+package com.tests;
+
+public class ExampleMain {
+	public static void main(String args[])
+	{
+		TestMain testmain = new TestMain();
+		testmain.add(10.5);
+		testmain.add(7,5);
+		testmain.add(10,5,0);
+		testmain.add(10.8,3.9);
+		
+		
+	}
+
+}
+ 
---
+++ polymorphisam/src/com/tests/TestMain.java
@@ -0,0 +1,25 @@
+
+package com.tests;
+
+public class TestMain 
+{
+	void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	void add(double a, double b)
+	{
+		System.out.println(a+b);
+		
+	}
+	void add(int a,int b,int c)
+	{
+		System.out.println(a+b+c);
+	}
+	void add(double a)
+	{
+		System.out.println(a);
+	}
+	
+
+}
